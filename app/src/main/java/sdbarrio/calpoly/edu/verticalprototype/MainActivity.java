db23b1eb7cb03c72ed362c3301c4f4abb1c6667f
Seth Barrios <sethb157@yahoo.com>
2016-10-30 19:32:05 -0700

    Location now working. Run app twice to make it function properly--first is for enabling location services

--- app/src/main/java/sdbarrio/calpoly/edu/verticalprototype/MainActivity.java
+++ app/src/main/java/sdbarrio/calpoly/edu/verticalprototype/MainActivity.java
@@ -1,13 +1,78 @@
 package sdbarrio.calpoly.edu.verticalprototype;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    public final int LOC_REQ_CODE = 35;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, LOC_REQ_CODE);
+        }
+        Location mlastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        String latitude, longitude;
+        if (mlastLocation != null) {
+            latitude = String.valueOf(mlastLocation.getLatitude());
+            longitude = String.valueOf(mlastLocation.getLongitude());
+            Log.d("MAIN_ACTIVITY", latitude + ", " + longitude);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("MAIN_ACTIVITY", "connection failed");
     }
 }
